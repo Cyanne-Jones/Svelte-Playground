53dc0f4802b8c08740f12193b980febedbf43fa8
Cyanne Jones <jonescyanne@yahoo.com>
2025-01-03 09:22:23 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+
+  const meow = $state(['m', 'e', 'o', 'w', '!'])
+
+  const meowLouder = () => {
+    meow.splice(2, 0, 'o');
+    // console.log($state.snapshot(booty));
+  }
+
+  $inspect(meow).with(() => console.log('Here!'));
+
+
+</script>
+
+<h1>{meow.join('')}</h1>
+<button onclick={meowLouder}>Meow louder</button>
+
+
+<style>
+  button {
+    cursor: pointer;
+  }
+</style>
